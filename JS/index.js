ac008dd7d366f46319b56e1f97b5686120ea28d5
Adeline Claire MOREL <adelinemorel72@gmail.com>
2021-04-18 10:05:04 +0200

    SASS_Modif_utilisation de flexbox pour repositionner dynamiquement les hashtags_JS_Ajout_base de l'algo_utilisation de fetch pour le json

---
+++ JS/index.js
@@ -0,0 +1,124 @@
+/*              *****COMMENT RECUPERER LES DONNEES DES PHOTOGRAPHES*****
+
+*** 1) Comment fonctionne une promesse en JavaScript ***
+
+//Création d'une promesse :
+const promise = new Promise((resolve, reject) => {
+    if(condition){
+        resolve("Ok, résolu")
+    }else{
+        reject("Bien essayé, pas résolu")
+    }
+});
+
+//Capter l'état de la promesse :
+promise
+    .then((dataResolve) => {
+        console.log(dataResolve); // -> donne "Ok, résolu"
+    })
+    .catch((dataReject) => {
+        console.log(dataReject); // -> donne "Bien essayé, pas résolu"
+    })*/
+
+
+//*** 2) Fetch : comment rédiger ***
+
+fetch('photographers.json')
+    .then((response) => { //récupère une promesse
+        return response.json(); //on formate la réponse au format json, pour dire que ce n'est pas simplement du contenu texte qu'on veut récupérer
+    })
+    .then((obj) => { //traitement des objets dans notre page web
+        console.log(obj);
+        //Est-ce que c'est ici que je dois écrire ce que je veux faire avec ces données ?
+    })
+    .catch((error) => {
+        console.error("Cela n'a pas fonctionné");
+        console.error(error);
+    });
+
+
+    //Ce que je veux faire: stocker l'objet dans une variable (exemple : const photographers = ...... ? -> je ne sais pas ce que je dois écrire ici, "obj" ?). Est-ce que je peux écrire ce que j'ai écrit plus haut mais directement dans une constante ?
+    //const photographers = fetch..... etc.
+
+//___________________________________________________________________________________________________________
+
+//                         ***** Méthode document.createElement() : *****
+
+
+/* Structure HTML à créer, qui se remplira de manière dynamique :
+`<article id="card${index +=1}" class="card">
+            <section class="card-profile">
+                <a href="">
+                    <figure>
+                        <img class="card-profile__img" src="${url -> je ne sais pas trop comment rendre ça dynamique}" alt="">
+                    </figure>
+                    <h2 class="card-profile__name">${name[index]}</h2>
+                </a>
+            </section>
+            <section class="card-info">
+                <p class="card-info__location">${city[index]}</p>
+                <p class="card-info__quote">${quote[index]}</p>
+                <p class="card-info__price">${price[index]}</p>
+            </section>
+            <section class="card-tags">
+                <ul>
+                    -> pour créer cette liste il faudra que je récupère dynamiquement celle de l'objet json, et plus besoin des id vu que j'ai fait du flex et que je vais mettre une width dynamique pour les tags. Je pense qu'il faudra que je fasse une boucle for (let tag of tags...).
+                    <li class="tags-links"><a href="">#${tag[]}</a></li>
+                    ...
+                    ...
+                </ul>
+            </section>
+        </article>`*/
+
+
+for (const photographer of obj.photographers){ //cela va énumérer les objets contenus dans obj
+    const createCard = addCard();
+    const createProfile = addProfileSection();
+    //il va falloir créer une boucle for in pour récupérer les paires clé/valeur de chaque objet photographer
+    createProfile.innerHTML = `<a href="">
+    <figure>
+        <img class="card-profile__img" src="images/${photographer[portrait]}" alt=""> 
+    </figure>
+    <h2 class="card-profile__name">${photographer[name]}</h2>
+    </a>`
+    //portait et name seront 2 variables stockant les valeurs des clés "portrait" et "name" dans photographer
+    const createInfo = addInfoSection();
+}
+
+function addCard(){
+    const newArticle = document.createElement("article"); //crée un nouvel élément <article>
+    newArticle.classList.add("card"); //ajoute la classe .card au nouvel article créé
+    document.body.appendChild(newArticle); //ajoute le nouvel article au body (à la suite de ce qui se trouve déjà dans le body)
+};
+
+function addProfileSection(){
+    const card = document.querySelector(".card"); //renverra true si une carte existe
+    const profile = document.querySelector(".card .card-profile") // vérifier si c'est bien comme ça qu'on écrit les query selector pour cibler card-profile dans card
+    //renverra true si un élément avec la classe card-profil existe dans la carte
+    const newProfile = document.createElement("section"); // crée un nouvel élement <section>
+    newProfile.classList.add("card-profile"); //ajoute la classe .card-profile à la nouvelle section créée
+    
+    if(card & !profile){ //à vérifier : je veux que la condition soit qu'une carte existe et qu'elle ne contienne pas encore de section de profil
+        document.card.appendChild(newProfile); //ajoute la nouvelle section avec la classe .card-profile à l'article
+    }else if(card & profile){ //si la carte contient déjà cette section
+        console.log("Cette carte contient déjà une section profil")
+    }else {
+        console.log("Vous devez d'abord créer une carte en utilisant la fonction addCard");
+    }
+};
+
+function addInfoSection(){
+    const card = document.querySelector(".card"); //renverra true si une carte existe
+    const info = document.querySelector(".card .card-info") // vérifier si c'est bien comme ça qu'on écrit les query selector pour cibler card-profile dans card
+    //renverra true si un élément avec la classe card-info existe dans la carte
+    const newInfo = document.createElement("section"); // crée un nouvel élement <section>
+    newInfo.classList.add("card-info"); //ajoute la classe .card-info à la nouvelle section créée
+    
+    if(card & !info){ //à vérifier : je veux que la condition soit qu'une carte existe et qu'elle ne contienne pas encore de section d'informations
+        document.card.appendChild(newInfo); //ajoute la nouvelle section avec la classe .card-info à l'article
+    }else if(card & info){ //si la carte contient déjà cette section
+        console.log("Cette carte contient déjà une section info")
+    }else {
+        console.log("Vous devez d'abord créer une carte en utilisant la fonction addCard");
+    }
+};
