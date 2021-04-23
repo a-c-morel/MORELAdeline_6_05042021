7aee54d7fe675a93a65368ee6264bf23a1e079c4
Adeline Claire MOREL <adelinemorel72@gmail.com>
2021-04-23 10:05:31 +0200

    JavaScript_Modif_tentative d'utilisation de la POO pour creer mes cartes_boucle ne fonctionne pas mais affichage premiere carte ok

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,113 @@ mainH1.classList.add("home-first-heading");
 mainH1.innerHTML = "Nos photographes";
 mainElement.appendChild(mainH1);
 
+const headerNav = document.querySelector("nav");
+const headerList = document.createElement("ul");
+headerNav.appendChild(headerList);
+
+/***Comment créer mes cartes en Programmation Orientée Objet***/
+
+//variables pour créer les balises (je les mets ici pour qu'elles soient accessibles depuis le scope global)
+const newArticle = document.createElement("article"); //crée un nouvel élément <article></article>
+const firstSection = document.createElement("section"); //crée un nouvel élement <section></section>
+const secondSection = document.createElement("section"); //crée un nouvel élement <section></section>
+const thirdSection = document.createElement("section"); //crée un nouvel élement <section></section>
+const firstSectionLink = document.createElement("a"); //crée un nouvel élément <a></a>
+const newFigure = document.createElement("figure"); //crée un nouvel élément <figure></figure>
+const newProfileImg = document.createElement("img"); //crée un nouvel élément <img></img>
+const newProfileTitle = document.createElement("h2"); //crée un nouvel élément <h2></h2>
+const locationParagraph = document.createElement("p"); //crée un nouvel élément <p></p>
+const quoteParagraph = document.createElement("p"); //crée un nouvel élément <p></p>
+const priceParagraph = document.createElement("p"); //crée un nouvel élément <p></p>
+const thirdSectionList = document.createElement("ul"); //crée un élément <ul></ul>
+const liTag = document.createElement("li"); //crée un nouvel élément <li></li>
+const hashtag = document.createElement("a"); //crée un élément <a></a>
+
+//Chaque carte sera une instance de la classe "card" :
+class Card{
+    constructor(portrait, name, city, country, tagline, price, tags){
+        this.portrait = portrait;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.tagline = tagline;
+        this.price = price;
+        this.tags = tags;
+    }
+
+    createCard(){
+        newArticle.classList.add("card"); //ajoute la classe .card au nouvel article que l'on veut ajouter
+        mainElement.appendChild(newArticle); //ajoute le nouvel article au main (à la suite de ce qui se trouve déjà dans le main)
+    }
+
+    createSections(){        
+        //donner la classe .card-profile à la première
+        firstSection.classList.add("card-profile");
+        //donner la classe .card-info à la deuxième
+        secondSection.classList.add("card-info");
+        //donner la classe .card-tags à la troisième
+        thirdSection.classList.add("card-tags");
+        //ajouter les 3 sections à l'article que l'on vient de créer plus haut 
+        newArticle.appendChild(firstSection);
+        newArticle.appendChild(secondSection);
+        newArticle.appendChild(thirdSection);
+    }
+
+    createProfileImg(){
+        //firstSectionLink.setAttribute("src", `page_du_photographe${variable}`); //A MODIFIER !
+        firstSection.appendChild(firstSectionLink); //ajoute l'élément <a></a> à la première section
+
+        firstSectionLink.appendChild(newFigure); //ajoute l'élément <figure></figure> à la première section de l'article
+
+        newProfileImg.classList.add("card-profile__img");
+        newProfileImg.setAttribute("src", `images/Photographers ID Photos/${this.portrait}`);
+        newFigure.appendChild(newProfileImg); //ajoute l'élément <img></img> à l'intérieur de <figure></figure>
+    }
+
+    createH2(){
+        newProfileTitle.classList.add("card-profile__name"); //ajoute la classe .card-profile__name au h2
+        newProfileTitle.innerHTML = `${this.name}`; //récupère le nom du photographe dans les paramètres de la carte et l'ajoute comme contenu du h2
+        firstSectionLink.appendChild(newProfileTitle); //ajoute le h2 après l'élément figure dans le <a></a>
+    }
+
+    createLocation(){
+        locationParagraph.classList.add("card-info__location"); //ajoute la classe .card-info__location
+        locationParagraph.innerHTML = `${this.city}, ${this.country}`; //récupère la ville et le pays dynamiquement dans les paramètres de la carte et les ajoute comme contenu du p
+        secondSection.appendChild(locationParagraph); //ajoute le p à la 2è section de la carte
+    }
+
+    createQuote(){
+        quoteParagraph.classList.add("card-info__quote"); //ajoute la classe .card-info__quote
+        quoteParagraph.innerHTML = `${this.tagline}`; //récupère la citation dans les paramètres de la carte et l'ajoute comme contenu du p
+        secondSection.appendChild(quoteParagraph); //ajoute le p après le précédent
+    }
+
+    createPrice(){
+        priceParagraph.classList.add("card-info__price"); //ajoute la classe .card-info__price
+        priceParagraph.innerHTML = `${this.price}/jour`; //récupère le prix dans les paramètres de la carte et l'ajoute comme contenu du p
+        secondSection.appendChild(priceParagraph); //ajoute le p après le précédent
+    }
+
+    createTags(){
+        thirdSection.appendChild(thirdSectionList); //ajoute le ul à la troisième section de la carte
+        for (const tag of photographer.tags){ //boucle qui parcourt le array qui contient les tags
+            liTag.classList.add("tags-links"); //ajoute la classe .tags-links au li
+            thirdSectionList.appendChild(liTag); //ajoute le li dans le ul de la 3e section
+
+            hashtag.setAttribute("href", ``); //ajoute l'attribut href à mon élément <a></a> //penser à remplir le href 
+            hashtag.innerHTML = `#${tag}`; //ajoute le contenu du tag à l'index de l'array où se trouve la boucle à ce moment
+            liTag.appendChild(hashtag); //ajoute le a avec son contenu dans le li
+        }
+    }
+}
+
+//Ensuite, pour créer une instance :
+
+//(à mettre dans la boucle du fetch)
+
+//let myCard = new card(photographer.portrait, photographer.name, photographer.city, photographer.country, photographer.tagline, photographer.price, photographer.tags);
+
+
 fetch('./JS/photographers.json')
     .then((response) => { //récupère une promesse
         return response.json(); //on formate la réponse au format json, pour dire que ce n'est pas simplement du contenu texte qu'on veut récupérer
@@ -13,7 +120,18 @@ fetch('./JS/photographers.json')
         console.log(obj);
         //Est-ce que c'est bien ici que je dois écrire ce que je veux faire avec ces données ?
         for (const photographer of obj.photographers){ //cela va énumérer les objets contenus dans obj
-                        
+            let myCard = new Card(photographer.portrait, photographer.name, photographer.city, photographer.country, photographer.tagline, photographer.price, photographer.tags);
+            myCard.createCard();
+            myCard.createSections();
+            myCard.createProfileImg();
+            myCard.createH2();
+            myCard.createLocation();
+            myCard.createQuote;
+            myCard.createPrice();
+            myCard.createTags();
+        }
+            
+            /*         
             //créer un article :
             const newArticle = document.createElement("article"); //crée un nouvel élément <article></article>
             newArticle.classList.add("card"); //ajoute la classe .card au nouvel article créé
@@ -35,7 +153,7 @@ fetch('./JS/photographers.json')
             newArticle.appendChild(thirdSection);
 
             const firstSectionLink = document.createElement("a"); //crée un nouvel élément <a></a>
-            /*firstSectionLink.setAttribute("src", `page_du_photographe${variable}`); //A MODIFIER !*/
+            firstSectionLink.setAttribute("src", `page_du_photographe${variable}`); //A MODIFIER !
             firstSection.appendChild(firstSectionLink); //ajoute l'élément <a></a> à la première section
 
             const newFigure = document.createElement("figure"); //crée un nouvel élément <figure></figure>
@@ -51,119 +169,55 @@ fetch('./JS/photographers.json')
             newProfileTitle.innerHTML = `${photographer.name}`; //récupère le nom du photographe dynamiquement dans les données JSON et l'ajoute comme contenu du h2
             firstSectionLink.appendChild(newProfileTitle); //ajoute le h2 après l'élément figure dans le <a></a>
 
-            const locationParagraph = document.createElement("p");
-            locationParagraph.classList.add("card-info__location");
-            locationParagraph.innerHTML = `${photographer.city}, ${photographer.country}`;
-            secondSection.appendChild(locationParagraph);
+            const locationParagraph = document.createElement("p"); //crée un nouvel élément <p></p>
+            locationParagraph.classList.add("card-info__location"); //ajoute la classe .card-info__location
+            locationParagraph.innerHTML = `${photographer.city}, ${photographer.country}`; //récupère la ville et le pays dynamiquement dans les données JSON et les ajoute comme contenu du p
+            secondSection.appendChild(locationParagraph); //ajoute le p à la 2è section de la carte
 
-            const quoteParagraph = document.createElement("p");
-            quoteParagraph.classList.add("card-info__quote");
-            quoteParagraph.innerHTML = `${photographer.tagline}`;
-            secondSection.appendChild(quoteParagraph);
+            const quoteParagraph = document.createElement("p"); //crée un nouvel élément <p></p>
+            quoteParagraph.classList.add("card-info__quote"); //ajoute la classe .card-info__quote
+            quoteParagraph.innerHTML = `${photographer.tagline}`; //récupère la citation dynamiquement dans les données JSON et l'ajoute comme contenu du p
+            secondSection.appendChild(quoteParagraph); //ajoute le p après le précédent
 
-            const priceParagraph = document.createElement("p");
-            priceParagraph.classList.add("card-info__price");
-            priceParagraph.innerHTML = `${photographer.price}/jour`;
-            secondSection.appendChild(priceParagraph);
+            const priceParagraph = document.createElement("p"); //crée un nouvel élément <p></p>
+            priceParagraph.classList.add("card-info__price"); //ajoute la classe .card-info__price
+            priceParagraph.innerHTML = `${photographer.price}/jour`; //récupère le prix dynamiquement dans les données JSON et l'ajoute comme contenu du p
+            secondSection.appendChild(priceParagraph); //ajoute le p après le précédent
 
-            const thirdSectionList = document.createElement("ul");
-            thirdSection.appendChild(thirdSectionList);
-            for (const tag of photographer.tags){
-                const liTag = document.createElement("li");
-                liTag.classList.add("tags-links");
-                thirdSectionList.appendChild(liTag);
+            const thirdSectionList = document.createElement("ul"); //crée un élément <ul></ul>
+            thirdSection.appendChild(thirdSectionList); //ajoute le ul à la troisième section de la carte
+            for (const tag of photographer.tags){ //boucle qui parcourt le array qui contient les tags
+                const liTag = document.createElement("li"); //crée un nouvel élément <li></li>
+                liTag.classList.add("tags-links"); //ajoute la classe .tags-links au li
+                thirdSectionList.appendChild(liTag); //ajoute le li dans le ul de la 3e section
 
-                const hashtag = document.createElement("a");
-                hashtag.setAttribute("href", ``); //à remplir plus tard
-                hashtag.innerHTML = `#${tag}`;
-                liTag.appendChild(hashtag);
-            }
-        }
+                const hashtag = document.createElement("a"); //crée un élément <a></a>
+                hashtag.setAttribute("href", ``); //ajoute l'attribut href à mon élément <a></a> //penser à remplir le href 
+                hashtag.innerHTML = `#${tag}`; //ajoute le contenu du tag à l'index de l'array où se trouve la boucle à ce moment
+                liTag.appendChild(hashtag); //ajoute le a avec son contenu dans le li
+            }*/
     })
     .catch((error) => {
         console.error("Cela n'a pas fonctionné");
         console.error(error);
     });
+  
+ 
+//Créer une boucle qui parcourt toutes les cartes pour en extraire les tags :
+
+const numberOfCards = mainElement.getElementsByTagName("article");
+console.log(numberOfCards.length); //donne 6
+
+/*for(let i=0; i < numberOfCards.length; i++){
+    - Créer un array qui stocke les tags existants
+    let hashtagsList = [];
+    hashtagsList.push(); -> cette étape m'embête car je ne sais pas très bien comment obtenir les tags... (ils sont maintenant existants dans le DOM donc je devrais pouvoir les cibler)
+        - Utiliser hashtable pour supprimer les doublons de mon array (faisable car ne contient que des types string, ça n'aurait pas été possible avec un array contenant des objets)
+
+        - Créer une boucle qui parcourt mon array
+
+            - à chaque tour de boucle, ajouter un li contenant le tag à cet index dans la nav (que je dois d'abord créer, vide)
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* Structure HTML à créer, qui se remplira de manière dynamique :
-`<article id="card${index +=1}" class="card">
-            <section class="card-profile">
-                <a href="">
-                    <figure>
-                        <img class="card-profile__img" src="${url -> je ne sais pas trop comment rendre ça dynamique}" alt="">
-                    </figure>
-                    <h2 class="card-profile__name">${name[index]}</h2>
-                </a>
-            </section>
-            <section class="card-info">
-                <p class="card-info__location">${city[index]}</p>
-                <p class="card-info__quote">${quote[index]}</p>
-                <p class="card-info__price">${price[index]}</p>
-            </section>
-            <section class="card-tags">
-                <ul>
-                    -> pour créer cette liste il faudra que je récupère dynamiquement celle de l'objet json, et plus besoin des id vu que j'ai fait du flex et que je vais mettre une width dynamique pour les tags. Je pense qu'il faudra que je fasse une boucle for (let tag of tags...).
-                    <li class="tags-links"><a href="">#${tag[]}</a></li>
-                    ...
-                    ...
-                </ul>
-            </section>
-        </article>`*/
-
-
-
-
-/*Ce que j'ai écrit ci-dessous ne va pas, je pense qu'il faut simplement que j'écrive une fonction qui génère la carte dans son intégralité et qui va chercher les valeurs dans le JSON*/
-
-/*function addProfileSection(){
-    const card = document.querySelector(".card"); //renverra true si une carte existe
-    const profile = document.querySelector(".card .card-profile") // vérifier si c'est bien comme ça qu'on écrit les query selector pour cibler card-profile dans card
-    //renverra true si un élément avec la classe card-profil existe dans la carte
-    const newProfile = document.createElement("section"); // crée un nouvel élement <section>
-    newProfile.classList.add("card-profile"); //ajoute la classe .card-profile à la nouvelle section créée
-    
-    if(card & !profile){ //à vérifier : je veux que la condition soit qu'une carte existe et qu'elle ne contienne pas encore de section de profil
-        document.card.appendChild(newProfile); //ajoute la nouvelle section avec la classe .card-profile à l'article
-    }else if(card & profile){ //si la carte contient déjà cette section
-        console.log("Cette carte contient déjà une section profil")
-    }else {
-        console.log("Vous devez d'abord créer une carte en utilisant la fonction addCard");
-    }
-};
-
-function addInfoSection(){
-    const card = document.querySelector(".card"); //renverra true si une carte existe
-    const info = document.querySelector(".card .card-info") // vérifier si c'est bien comme ça qu'on écrit les query selector pour cibler card-profile dans card
-    //renverra true si un élément avec la classe card-info existe dans la carte
-    const newInfo = document.createElement("section"); // crée un nouvel élement <section>
-    newInfo.classList.add("card-info"); //ajoute la classe .card-info à la nouvelle section créée
-    
-    if(card & !info){ //à vérifier : je veux que la condition soit qu'une carte existe et qu'elle ne contienne pas encore de section d'informations
-        document.card.appendChild(newInfo); //ajoute la nouvelle section avec la classe .card-info à l'article
-    }else if(card & info){ //si la carte contient déjà cette section
-        console.log("Cette carte contient déjà une section info")
-    }else {
-        console.log("Vous devez d'abord créer une carte en utilisant la fonction addCard");
-    }
-};*/
+     */
