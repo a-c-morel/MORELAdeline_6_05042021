39cf1e45a12b92fbab4a7964b59e234d1a942da1
Adeline Claire MOREL <adelinemorel72@gmail.com>
2021-05-15 08:51:44 +0200

    JavaScript_Modif_classe Picture pour pouvoir creer des instances pour les images des galeries des photographes

--- JS/picture.js
+++ JS/picture.js
@@ -5,14 +5,21 @@ class Picture {
         this.numberOfLikes = numberOfLikes;
     }
     display(){
-        /*<article>
-                <figure>
-                    <img src="" alt="">
-                    <div>
-                        <p>texte de la photo</p>
-                        <i>icone like</i>
-                    </div>
-                </figure>
-            </article> */
+        const pictureArticle = document.createElement("article");
+        const pictureSection = document.createElement("section");
+        const pictureInfoSection = document.createElement("section");
+        const pictureImg = document.createElement("img");
+        const pictureTitle = document.createElement("p");
+        const pictureLikes = document.createElement("i");
+
+        pictureImg.setAttribute("src", `${this.img}`);
+        pictureTitle.innerHTML = `${this.title}`;
+        pictureLikes.innerHTML = `${this.numberOfLikes}`;
+
+        pictureArticle.appendChild(pictureSection);
+        pictureArticle.appendChild(pictureInfoSection);
+        pictureSection.appendChild(pictureImg);
+        pictureInfoSection.appendChild(pictureTitle);
+        pictureInfoSection.appendChild(pictureLikes);
     }
 }
