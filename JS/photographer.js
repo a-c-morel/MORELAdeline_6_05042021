24b113284896d65ac05d7012abd82a2eda9a07ed
Adeline Claire MOREL <adelinemorel72@gmail.com>
2021-05-28 11:33:13 +0200

    SCSS_Ajout_début de mise en forme pour les sections de profil pour chaque page de photographe

--- JS/photographer.js
+++ JS/photographer.js
@@ -101,55 +101,50 @@ class Profile extends PhotographerFactory{
     }
     display(){
         //create elements
-        const sectionContainer = document.createElement("section");
         const newArticle = document.createElement("article");
         const firstSection = document.createElement("section");
         const firstSectionH1 = document.createElement("h1");
-        const secondSection = document.createElement("section");
         const locationParagraph = document.createElement("p");
         const quoteParagraph = document.createElement("p");
-        const thirdSection = document.createElement("section");
-        const thirdSectionList = document.createElement("ul");
-        const fourthSection = document.createElement("section");
+        const tagsSection = document.createElement("section");
+        const tagsSectionList = document.createElement("ul");
+        const secondSection = document.createElement("section");
         const newDiv = document.createElement("div");
         const newProfileImg = document.createElement("img");
 
         //add classes and attributes + content
-        newArticle.classList.add("card");
-        firstSection.classList.add("card-profile");
-        firstSectionH1.classList.add("card-profile__name");
+        newArticle.classList.add("profile"); //dans le scss, je pourrai prendre les propriétés que j'ai appliquées à .card et les modifier un peu
+        firstSection.classList.add("profile-info");
+        firstSectionH1.classList.add("profile-info__name");
         firstSectionH1.innerHTML = `${this.name}`;
-        secondSection.classList.add("card-info");
-        locationParagraph.classList.add("card-info__location");
+        locationParagraph.classList.add("profile-info__location");
         locationParagraph.innerHTML = `${this.city}, ${this.country}`;
-        quoteParagraph.classList.add("card-info__quote");
+        quoteParagraph.classList.add("profile-info__quote");
         quoteParagraph.innerHTML = `${this.tagline}`;
-        newProfileImg.classList.add("card-profile__img");
+        secondSection.classList.add("profile-image");
         newProfileImg.setAttribute("src", `images/Photographers ID Photos/${this.portrait}`);
 
         //add elements to their parents
-        mainElement.appendChild(sectionContainer);
-        sectionContainer.appendChild(newArticle);
+        mainElement.appendChild(newArticle);
         newArticle.appendChild(firstSection);
-        newArticle.appendChild(secondSection);        
-        newArticle.appendChild(thirdSection);
-        newArticle.appendChild(fourthSection);
+        newArticle.appendChild(secondSection);
         firstSection.appendChild(firstSectionH1);
-        secondSection.appendChild(locationParagraph);
-        secondSection.appendChild(quoteParagraph);
-        thirdSection.appendChild(thirdSectionList);
-        fourthSection.appendChild(newDiv);
+        firstSection.appendChild(locationParagraph);
+        firstSection.appendChild(quoteParagraph);
+        firstSection.appendChild(tagsSection);
+        tagsSection.appendChild(tagsSectionList);
+        secondSection.appendChild(newDiv);
         newDiv.appendChild(newProfileImg);
         
         for (const tag of this.tags){
             const liTag = document.createElement("li");
             const hashtag = document.createElement("a");
             liTag.classList.add("tags-links");
-            thirdSectionList.appendChild(liTag);
+            tagsSectionList.appendChild(liTag);
             hashtag.setAttribute("href", ``);
             hashtag.innerHTML = `<span aria-hidden="true">#</span>${tag}`;
             liTag.appendChild(hashtag);
         }
-        return sectionContainer;
+        return newArticle;
     }
 }
