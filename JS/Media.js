94105e505dfae194ba60e14046d765d4d7db4c3d
Adeline Claire MOREL <adelinemorel72@gmail.com>
2021-06-21 16:45:43 +0200

    JavaScript_Modif_methode display de la classe Image_changement de la structure HTML et modif noms de classes plus pertinents

--- JS/Media.js
+++ JS/Media.js
@@ -12,6 +12,74 @@ class MediaFactory{
     }
 }
 
+class Image{
+    constructor(props){
+        this.id = props.id;
+        this.url = props.url;
+        this.title = props.title;
+        this.photographerId = props.photographerId;
+        this.tags = props.tags;
+        this.likes = props.likes;
+        this.date = props.date;
+        this.price = props.price
+    }
+    display(){
+        const newImageArticle = document.createElement("article");
+        newImageArticle.classList.add("media");
+
+        const newMediaSection = document.createElement("section");
+        newMediaSection.classList.add("media-preview");
+
+        const newInfoSection = document.createElement("section");
+        newInfoSection.classList.add("media-info");
+
+
+        const newImage = document.createElement("img");
+        newImage. classList.add("media-preview__media");
+        newImage.setAttribute("src", `images/medias/${this.url}`);
+
+        const newImageTitle = document.createElement("h2");
+        newImageTitle.classList.add("media-info__title");
+        newImageTitle.innerHTML = `${this.title}`;
+
+        const newImageLikes = document.createElement("p");
+        newImageLikes.classList.add("media-info__likes");
+
+        //mainElement.appendChild(newImageArticle);
+        newImageArticle.appendChild(newMediaSection);
+        newImageArticle.appendChild(newInfoSection);
+        newMediaSection.appendChild(newImage);
+        newInfoSection.appendChild(newImageTitle);
+        newInfoSection.appendChild(newImageLikes);
+
+        /*const newImageArticle = document.createElement("article");
+        const newImageLink = document.createElement("a");
+        const newImage = document.createElement("img");
+        const newImageInfoSection = document.createElement("section");
+        const newImageTitle = document.createElement("p");
+        const newImageLikes = document.createElement("p");
+        
+        newImageArticle.classList.add("gallery");
+        newImageLink.classList.add("gallery-link");
+        newImage.setAttribute("src", `images/medias/${this.url}`);
+        newImage.classList.add("gallery-link__media");
+        newImageInfoSection.classList.add("gallery-info");
+        newImageTitle.innerHTML = `${this.title}`;
+        newImageTitle.classList.add("gallery-info__title");
+        newImageLikes.classList.add("gallery-info__likes")
+        newImageLikes.innerHTML = `${this.likes}`;
+        
+        mainElement.appendChild(newImageArticle);
+        newImageArticle.appendChild(newImageLink);
+        newImageLink.appendChild(newImage);
+        newImageArticle.appendChild(newImageInfoSection);
+        newImageInfoSection.appendChild(newImageTitle);
+        newImageInfoSection.appendChild(newImageLikes);*/
+        
+        return newImageArticle;
+    }
+}
+
 class Video{
     constructor(props){
         this.id = props.id;
@@ -24,29 +92,29 @@ class Video{
         this.price = props.price
     }
     display(){
-        const newVideoArticle = document.createElement("article") ;
-        newVideoArticle.classList.add("media") ;
-        const newMediaSection = document.createElement("section") ;
-        newMediaSection.classList.add("media-preview") ;
-        const newInfoSection = document.createElement("section") ;
-        newInfoSection.classList.add("media-info") ;
-        const newVideo = document.createElement("video") ;
-        newVideo. classList.add("media-preview__media") ;
-        const newSource = document.createElement("source") ;
+        const newVideoArticle = document.createElement("article");
+        newVideoArticle.classList.add("media");
+        const newMediaSection = document.createElement("section");
+        newMediaSection.classList.add("media-preview");
+        const newInfoSection = document.createElement("section");
+        newInfoSection.classList.add("media-info");
+        const newVideo = document.createElement("video");
+        newVideo. classList.add("media-preview__media");
+        const newSource = document.createElement("source");
         newSource.setAttribute("src", `images/medias/${this.url}`);
-        const newVideoTitle = document.createElement("h2") ;
-        newVideoTitle.classList.add(" media-info__title") ;
+        const newVideoTitle = document.createElement("h2");
+        newVideoTitle.classList.add("media-info__title");
         newVideoTitle.innerHTML = `${this.title}`;
-        const newVideoLikes = document.createElement("p") ;
-        newVideoLikes.classList.add("media-info__likes") ;
+        const newVideoLikes = document.createElement("p");
+        newVideoLikes.classList.add("media-info__likes");
 
         //mainElement.appendChild(newVideoArticle);
-        newVideoArticle.appendChild( newMediaSection) ;
-        newVideoArticle.appendChild( newInfoSection) ;
-        newMediaSection.appendChild(newVideo) ;
-        newVideo.appendChild(newSource) ;
-        newInfoSection.appendChild(newVideoTitle) ;
-        newInfoSection.appendChild(newVideoLikes) ;
+        newVideoArticle.appendChild(newMediaSection);
+        newVideoArticle.appendChild(newInfoSection);
+        newMediaSection.appendChild(newVideo);
+        newVideo.appendChild(newSource);
+        newInfoSection.appendChild(newVideoTitle);
+        newInfoSection.appendChild(newVideoLikes);
         /*const newVideoArticle = document.createElement("article");
         const newVideoLink = document.createElement("a");
         const newVideoThumbnail = document.createElement("video");
@@ -75,46 +143,5 @@ class Video{
         //lightbox.appendChild(lightboxVideo);*/
         
         return newVideoArticle;
-    }
-}
-
-class Image{
-    constructor(props){
-        this.id = props.id;
-        this.url = props.url;
-        this.title = props.title;
-        this.photographerId = props.photographerId;
-        this.tags = props.tags;
-        this.likes = props.likes;
-        this.date = props.date;
-        this.price = props.price
-    }
-    display(){
-        //ce que doit faire la méthode pour générer un objet de type photo
-        const newImageArticle = document.createElement("article");
-        const newImageLink = document.createElement("a");
-        const newImage = document.createElement("img");
-        const newImageInfoSection = document.createElement("section");
-        const newImageTitle = document.createElement("p");
-        const newImageLikes = document.createElement("p");
-        
-        newImageArticle.classList.add("gallery");
-        newImageLink.classList.add("gallery-link");
-        newImage.setAttribute("src", `images/medias/${this.url}`);
-        newImage.classList.add("gallery-link__media");
-        newImageInfoSection.classList.add("gallery-info");
-        newImageTitle.innerHTML = `${this.title}`;
-        newImageTitle.classList.add("gallery-info__title");
-        newImageLikes.classList.add("gallery-info__likes")
-        newImageLikes.innerHTML = `${this.likes}`;
-        
-        mainElement.appendChild(newImageArticle);
-        newImageArticle.appendChild(newImageLink);
-        newImageLink.appendChild(newImage);
-        newImageArticle.appendChild(newImageInfoSection);
-        newImageInfoSection.appendChild(newImageTitle);
-        newImageInfoSection.appendChild(newImageLikes);
-        
-        return newImageArticle;
     }
 }
